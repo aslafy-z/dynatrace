87e4c4528591c5c9ee2e202319a7b8caf2569317
come.to.the.dark.side.cookie@gmail.com <reinhard.pilz@dynatrace.com>
2020-11-20 15:29:02 +0100

    Update method_rule.go

--- api/config/customservices/method_rule.go
+++ api/config/customservices/method_rule.go
@@ -2,8 +2,8 @@ package customservices
 
 // MethodRule TODO: documentation
 type MethodRule struct {
-	ID            string   `json:"id,omitempty"`            // The ID of the method rule
-	MethodName    string   `json:"methodName"`              // The method to instrument
-	ArgumentTypes []string `json:"argumentTypes,omitempty"` // Fully qualified types of argument the method expects
-	ReturnType    string   `json:"returnType"`              // Fully qualified type the method returns
+	ID            string   `json:"id,omitempty"`  // The ID of the method rule
+	MethodName    string   `json:"methodName"`    // The method to instrument
+	ArgumentTypes []string `json:"argumentTypes"` // Fully qualified types of argument the method expects
+	ReturnType    string   `json:"returnType"`    // Fully qualified type the method returns
 }
