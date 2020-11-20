ff4ade727d3d076ca7944cc62ce7b603e5e76376
come.to.the.dark.side.cookie@gmail.com <reinhard.pilz@dynatrace.com>
2020-11-20 13:32:39 +0100

    Update detection_rule.go

--- api/config/customservices/detection_rule.go
+++ api/config/customservices/detection_rule.go
@@ -4,7 +4,7 @@ package customservices
 type DetectionRule struct {
 	ID               string           `json:"id,omitempty"`              // The ID of the detection rule
 	Enabled          bool             `json:"enabled"`                   // Rule enabled/disabled
-	FileName         string           `json:"fileName,omitempty"`        // The PHP file containing the class or methods to instrument. Required for PHP custom service. Not applicable to Java and .NET
+	FileName         string           `json:"fileName"`                  // The PHP file containing the class or methods to instrument. Required for PHP custom service. Not applicable to Java and .NET
 	FileNameMatcher  FileNameMatcher  `json:"fileNameMatcher,omitempty"` // Matcher applying to the file name. Default value is `ENDS_WITH` (if applicable)
 	ClassName        string           `json:"className,omitempty"`       // The fully qualified class or interface to instrument. Required for Java and .NET custom services. Not applicable to PHP
 	ClassNameMatcher ClassNameMatcher `json:"matcher,omitempty"`         // Matcher applying to the class name. `STARTS_WITH` can only be used if there is at least one annotation defined. Default value is `EQUALS`
