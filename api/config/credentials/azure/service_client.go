a9e508fcbe445a0c38e29a6e87a6736b9a370c92
come.to.the.dark.side.cookie@gmail.com <reinhard.pilz@dynatrace.com>
2021-04-22 12:38:30 +0200

    switched to manual serialization

--- api/config/credentials/azure/service_client.go
+++ api/config/credentials/azure/service_client.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"log"
 
 	api "github.com/dtcookie/dynatrace/api/config"
 	"github.com/dtcookie/dynatrace/rest"
@@ -38,7 +37,6 @@ func (cs *ServiceClient) Create(config *AzureCredentials) (*api.EntityShortRepre
 	}
 
 	if bytes, err = cs.client.POST("/azure/credentials", config, 201); err != nil {
-		log.Fatal(err)
 		return nil, err
 	}
 	var stub api.EntityShortRepresentation
