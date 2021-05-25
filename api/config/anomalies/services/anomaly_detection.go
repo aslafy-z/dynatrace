d91311915e11f470520d748570e53e3eebc0b0af
come.to.the.dark.side.cookie@gmail.com <reinhard.pilz@dynatrace.com>
2021-05-25 12:02:49 +0200

    outsourced anomalies/common packages

--- api/config/anomalies/services/anomaly_detection.go
+++ api/config/anomalies/services/anomaly_detection.go
@@ -4,21 +4,21 @@ import (
 	"encoding/json"
 
 	api "github.com/dtcookie/dynatrace/api/config"
-	"github.com/dtcookie/dynatrace/api/config/anomalies/services/failurerateincrease"
-	"github.com/dtcookie/dynatrace/api/config/anomalies/services/load"
-	"github.com/dtcookie/dynatrace/api/config/anomalies/services/responsetimedegredation"
+	"github.com/dtcookie/dynatrace/api/config/anomalies/common/detection"
+	"github.com/dtcookie/dynatrace/api/config/anomalies/common/failurerate"
+	"github.com/dtcookie/dynatrace/api/config/anomalies/common/load"
+	"github.com/dtcookie/dynatrace/api/config/anomalies/common/responsetime"
 	"github.com/dtcookie/hcl"
 	"github.com/dtcookie/xjson"
 )
 
 // AnomalyDetection Dynatrace automatically detects service-related performance anomalies such as response time degradations and failure rate increases. Use these settings to configure detection sensitivity, set alert thresholds, or disable alerting for certain services.
 type AnomalyDetection struct {
-	LoadSpike               *load.SpikeDetection               `json:"loadSpike,omitempty"`     // The configuration of load spikes detection.
-	ResponseTimeDegradation *responsetimedegredation.Detection `json:"responseTimeDegradation"` // Configuration of response time degradation detection.
-	FailureRateIncrease     *failurerateincrease.Detection     `json:"failureRateIncrease"`     // Configuration of failure rate increase detection.
-	LoadDrop                *load.DropDetection                `json:"loadDrop,omitempty"`      // The configuration of load drops detection.
-	Metadata                *api.ConfigMetadata                `json:"metadata,omitempty"`      // Metadata useful for debugging
-	Unknowns                map[string]json.RawMessage         `json:"-"`
+	LoadSpike               *load.SpikeDetection    `json:"loadSpike,omitempty"`     // The configuration of load spikes detection.
+	ResponseTimeDegradation *responsetime.Detection `json:"responseTimeDegradation"` // Configuration of response time degradation detection.
+	FailureRateIncrease     *failurerate.Detection  `json:"failureRateIncrease"`     // Configuration of failure rate increase detection.
+	LoadDrop                *load.DropDetection     `json:"loadDrop,omitempty"`      // The configuration of load drops detection.
+	Metadata                *api.ConfigMetadata     `json:"metadata,omitempty"`      // Metadata useful for debugging
 }
 
 func (me *AnomalyDetection) Schema() map[string]*hcl.Schema {
@@ -27,34 +27,29 @@ func (me *AnomalyDetection) Schema() map[string]*hcl.Schema {
 			Type:        hcl.TypeList,
 			Optional:    true,
 			MaxItems:    1,
-			Description: "The configuration of load spikes detection",
+			Description: "The configuration of load spikes detection. Detecting load spikes will be disabled if this block is omitted.",
 			Elem:        &hcl.Resource{Schema: new(load.SpikeDetection).Schema()},
 		},
 		"response_times": {
 			Type:        hcl.TypeList,
 			Optional:    true,
 			MaxItems:    1,
-			Description: "Configuration of response time degradation detection",
-			Elem:        &hcl.Resource{Schema: new(responsetimedegredation.Detection).Schema()},
+			Description: "Configuration of response time degradation detection. Detecting response time degradation will be disabled if this block is omitted.",
+			Elem:        &hcl.Resource{Schema: new(responsetime.Detection).Schema()},
 		},
 		"failure_rates": {
 			Type:        hcl.TypeList,
 			Optional:    true,
 			MaxItems:    1,
-			Description: "Configuration of response time degradation detection",
-			Elem:        &hcl.Resource{Schema: new(failurerateincrease.Detection).Schema()},
+			Description: "Configuration of failure rate increase detection. Detecting failure rate increases will be disabled if this block is omitted.",
+			Elem:        &hcl.Resource{Schema: new(failurerate.Detection).Schema()},
 		},
 		"load_drops": {
 			Type:        hcl.TypeList,
 			Optional:    true,
 			MaxItems:    1,
-			Description: "The configuration of load drops detection",
+			Description: "The configuration of load drops detection. Detecting load drops will be disabled if this block is omitted.",
 			Elem:        &hcl.Resource{Schema: new(load.DropDetection).Schema()},
-		},
-		"unknowns": {
-			Type:        hcl.TypeString,
-			Description: "allows for configuring properties that are not explicitly supported by the current version of this provider",
-			Optional:    true,
 		},
 	}
 }
@@ -62,35 +57,28 @@ func (me *AnomalyDetection) Schema() map[string]*hcl.Schema {
 func (me *AnomalyDetection) MarshalHCL(decoder hcl.Decoder) (map[string]interface{}, error) {
 	result := map[string]interface{}{}
 
-	if len(me.Unknowns) > 0 {
-		data, err := json.Marshal(me.Unknowns)
-		if err != nil {
-			return nil, err
-		}
-		result["unknowns"] = string(data)
-	}
-	if me.LoadSpike != nil {
+	if me.LoadSpike != nil && me.LoadSpike.Enabled {
 		if marshalled, err := me.LoadSpike.MarshalHCL(hcl.NewDecoder(decoder, "load_spikes", 0)); err == nil {
 			result["load_spikes"] = []interface{}{marshalled}
 		} else {
 			return nil, err
 		}
 	}
-	if me.ResponseTimeDegradation != nil {
+	if me.ResponseTimeDegradation != nil && string(me.ResponseTimeDegradation.DetectionMode) != string(detection.Modes.DontDetect) {
 		if marshalled, err := me.ResponseTimeDegradation.MarshalHCL(hcl.NewDecoder(decoder, "response_times", 0)); err == nil {
 			result["response_times"] = []interface{}{marshalled}
 		} else {
 			return nil, err
 		}
 	}
-	if me.FailureRateIncrease != nil {
+	if me.FailureRateIncrease != nil && string(me.FailureRateIncrease.DetectionMode) != string(detection.Modes.DontDetect) {
 		if marshalled, err := me.FailureRateIncrease.MarshalHCL(hcl.NewDecoder(decoder, "failure_rates", 0)); err == nil {
 			result["failure_rates"] = []interface{}{marshalled}
 		} else {
 			return nil, err
 		}
 	}
-	if me.LoadDrop != nil {
+	if me.LoadDrop != nil && me.LoadDrop.Enabled {
 		if marshalled, err := me.LoadDrop.MarshalHCL(hcl.NewDecoder(decoder, "load_drops", 0)); err == nil {
 			result["load_drops"] = []interface{}{marshalled}
 		} else {
@@ -101,52 +89,43 @@ func (me *AnomalyDetection) MarshalHCL(decoder hcl.Decoder) (map[string]interfac
 }
 
 func (me *AnomalyDetection) UnmarshalHCL(decoder hcl.Decoder) error {
-	if value, ok := decoder.GetOk("unknowns"); ok {
-		if err := json.Unmarshal([]byte(value.(string)), me); err != nil {
-			return err
-		}
-		if err := json.Unmarshal([]byte(value.(string)), &me.Unknowns); err != nil {
-			return err
-		}
-		delete(me.Unknowns, "loadSpike")
-		delete(me.Unknowns, "responseTimeDegradation")
-		delete(me.Unknowns, "failureRateIncrease")
-		delete(me.Unknowns, "loadDrop")
-		delete(me.Unknowns, "metadata")
-		if len(me.Unknowns) == 0 {
-			me.Unknowns = nil
-		}
-	}
-
 	if _, ok := decoder.GetOk("load_spikes.#"); ok {
 		me.LoadSpike = new(load.SpikeDetection)
 		if err := me.LoadSpike.UnmarshalHCL(hcl.NewDecoder(decoder, "load_spikes", 0)); err != nil {
 			return err
 		}
+	} else {
+		me.LoadSpike = &load.SpikeDetection{Enabled: false}
 	}
 	if _, ok := decoder.GetOk("response_times.#"); ok {
-		me.ResponseTimeDegradation = new(responsetimedegredation.Detection)
+		me.ResponseTimeDegradation = new(responsetime.Detection)
 		if err := me.ResponseTimeDegradation.UnmarshalHCL(hcl.NewDecoder(decoder, "response_times", 0)); err != nil {
 			return err
 		}
+	} else {
+		me.ResponseTimeDegradation = &responsetime.Detection{DetectionMode: detection.Modes.DontDetect}
 	}
 	if _, ok := decoder.GetOk("failure_rates.#"); ok {
-		me.FailureRateIncrease = new(failurerateincrease.Detection)
+		me.FailureRateIncrease = new(failurerate.Detection)
 		if err := me.FailureRateIncrease.UnmarshalHCL(hcl.NewDecoder(decoder, "failure_rates", 0)); err != nil {
 			return err
 		}
+	} else {
+		me.FailureRateIncrease = &failurerate.Detection{DetectionMode: detection.Modes.DontDetect}
 	}
 	if _, ok := decoder.GetOk("load_drops.#"); ok {
 		me.LoadDrop = new(load.DropDetection)
 		if err := me.LoadDrop.UnmarshalHCL(hcl.NewDecoder(decoder, "load_drops", 0)); err != nil {
 			return err
 		}
+	} else {
+		me.LoadDrop = &load.DropDetection{Enabled: false}
 	}
 	return nil
 }
 
 func (me *AnomalyDetection) MarshalJSON() ([]byte, error) {
-	properties := xjson.NewProperties(me.Unknowns)
+	properties := xjson.Properties{}
 	if err := properties.MarshalAll(map[string]interface{}{
 		"loadSpike":               me.LoadSpike,
 		"responseTimeDegradation": me.ResponseTimeDegradation,
@@ -172,9 +151,6 @@ func (me *AnomalyDetection) UnmarshalJSON(data []byte) error {
 		"metadata":                &me.Metadata,
 	}); err != nil {
 		return err
-	}
-	if len(properties) > 0 {
-		me.Unknowns = properties
 	}
 	return nil
 }
--- api/config/anomalies/services/go.mod
+++ api/config/anomalies/services/go.mod
@@ -4,8 +4,11 @@ go 1.15
 
 require (
 	github.com/dtcookie/dynatrace/api/config v1.0.4
+	github.com/dtcookie/dynatrace/api/config/anomalies/common v1.0.0
 	github.com/dtcookie/dynatrace/rest v1.0.14
 	github.com/dtcookie/hcl v0.0.8
 	github.com/dtcookie/opt v1.0.0
 	github.com/dtcookie/xjson v1.0.2
 )
+
+replace github.com/dtcookie/hcl => C:\Users\Reinhard.Pilz\go\src\github.com\dtcookie\hcl
