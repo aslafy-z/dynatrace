1b8e4d7e9da2d2341fdea0a74c8e30f832f5fc29
come.to.the.dark.side.cookie@gmail.com <reinhard.pilz@dynatrace.com>
2021-05-28 10:59:59 +0200

    updated to latest dependencies

--- api/config/maintenancewindows/tag_info.go
+++
@@ -1,8 +0,0 @@
-package maintenancewindows
-
-// TagInfo Tag of a Dynatrace entity.
-type TagInfo struct {
-	Context Context `json:"context"`         // The origin of the tag, such as AWS or Cloud Foundry.   Custom tags use the `CONTEXTLESS` value.
-	Key     string  `json:"key"`             // The key of the tag.   Custom tags have the tag value here.
-	Value   *string `json:"value,omitempty"` // The value of the tag.   Not applicable to custom tags.
-}
