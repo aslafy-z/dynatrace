71cc347eba48ca799ef8457bc0c6db62eacbc5b2
come.to.the.dark.side.cookie@gmail.com <reinhard.pilz@dynatrace.com>
2021-05-28 11:26:02 +0200

    updated to latest dependencies

--- api/config/anomalies/diskevents/anomaly_detection.go
+++ api/config/anomalies/diskevents/anomaly_detection.go
@@ -3,6 +3,8 @@ package diskevents
 import (
 	api "github.com/dtcookie/dynatrace/api/config"
 	"github.com/dtcookie/dynatrace/api/config/common"
+	"github.com/dtcookie/hcl"
+	"github.com/dtcookie/opt"
 )
 
 // AnomalyDetection has no documentation
@@ -16,8 +18,98 @@ type AnomalyDetection struct {
 	Samples          int32               `json:"samples"`                  // The number of samples to evaluate.
 	ViolatingSamples int32               `json:"violatingSamples"`         // The number of samples that must violate the threshold to trigger an event. Must not exceed the number of evaluated samples.
 	Metric           Metric              `json:"metric"`                   // The metric to monitor.
-	TagFilters       []*common.TagFilter `json:"tagFilters,omitempty"`     // Narrows the rule usage down to the hosts matching the specified tags.
+	TagFilters       common.TagFilters   `json:"tagFilters,omitempty"`     // Narrows the rule usage down to the hosts matching the specified tags.
 	Metadata         *api.ConfigMetadata `json:"metadata,omitempty"`       // Metadata useful for debugging
+}
+
+func (me *AnomalyDetection) Schema() map[string]*hcl.Schema {
+	return map[string]*hcl.Schema{
+		"name": {
+			Type:        hcl.TypeString,
+			Required:    true,
+			Description: "The name of the disk event rule",
+		},
+		"host_group_id": {
+			Type:        hcl.TypeString,
+			Optional:    true,
+			Description: "Narrows the rule usage down to disks that run on hosts that themselves run on the specified host group",
+		},
+		"threshold": {
+			Type:        hcl.TypeFloat,
+			Required:    true,
+			Description: "The threshold to trigger disk event.   * A percentage for `LowDiskSpace` or `LowInodes` metrics.   * In milliseconds for `ReadTimeExceeding` or `WriteTimeExceeding` metrics",
+		},
+		"disk_name": {
+			Type:        hcl.TypeList,
+			Optional:    true,
+			MaxItems:    1,
+			Description: "Narrows the rule usage down to disks, matching the specified criteria",
+			Elem:        &hcl.Resource{Schema: new(DiskNameFilter).Schema()},
+		},
+		"enabled": {
+			Type:        hcl.TypeBool,
+			Required:    true,
+			Description: "Disk event rule enabled/disabled",
+		},
+		"samples": {
+			Type:        hcl.TypeInt,
+			Required:    true,
+			Description: "The number of samples to evaluate",
+		},
+		"violating_samples": {
+			Type:        hcl.TypeInt,
+			Required:    true,
+			Description: "The number of samples that must violate the threshold to trigger an event. Must not exceed the number of evaluated samples",
+		},
+		"metric": {
+			Type:        hcl.TypeString,
+			Required:    true,
+			Description: "The metric to monitor",
+		},
+		"tags": {
+			Type:        hcl.TypeList,
+			Optional:    true,
+			MaxItems:    1,
+			Description: "Narrows the rule usage down to the hosts matching the specified tags",
+			Elem:        &hcl.Resource{Schema: new(common.TagFilters).Schema()},
+		},
+	}
+}
+
+func (me *AnomalyDetection) MarshalHCL(decoder hcl.Decoder) (map[string]interface{}, error) {
+	return decoder.MarshalAll(map[string]interface{}{
+		"name":              me.Name,
+		"host_group_id":     me.HostGroupID,
+		"threshold":         me.Threshold,
+		"enabled":           me.Enabled,
+		"violating_samples": me.ViolatingSamples,
+		"metric":            me.Metric,
+		"disk_name":         me.DiskNameFilter,
+		"tags":              me.TagFilters,
+	})
+}
+
+func (me *AnomalyDetection) UnmarshalHCL(decoder hcl.Decoder) error {
+	reader := hcl.NewReader(decoder, nil)
+	me.Name = opt.String(reader.String("name"))
+	me.HostGroupID = reader.String("host_group_id")
+	me.Threshold = opt.Float64(reader.Float64("threshold"))
+	me.Enabled = opt.Bool(reader.Bool("enabled"))
+	me.ViolatingSamples = opt.Int32(reader.Int32("violating_samples"))
+	me.Metric = Metric(opt.String(reader.String("metric")))
+	if _, ok := decoder.GetOk("disk_name.#"); ok {
+		me.DiskNameFilter = new(DiskNameFilter)
+		if err := me.DiskNameFilter.UnmarshalHCL(hcl.NewDecoder(decoder, "disk_name", 0)); err != nil {
+			return err
+		}
+	}
+	if _, ok := decoder.GetOk("tags.#"); ok {
+		me.TagFilters = common.TagFilters{}
+		if err := me.TagFilters.UnmarshalHCL(hcl.NewDecoder(decoder, "tags", 0)); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 // Metric The metric to monitor.
--- api/config/anomalies/diskevents/go.mod
+++ api/config/anomalies/diskevents/go.mod
@@ -3,6 +3,9 @@ module github.com/dtcookie/dynatrace/api/config/anomalies/diskevents
 go 1.15
 
 require (
-	github.com/dtcookie/dynatrace/api/config v1.0.5
-	github.com/dtcookie/dynatrace/api/config/common v1.0.0
+	github.com/dtcookie/dynatrace/api/config v1.0.6
+	github.com/dtcookie/dynatrace/api/config/common v1.0.1
+	github.com/dtcookie/dynatrace/rest v1.0.14
+	github.com/dtcookie/hcl v0.0.10
+	github.com/dtcookie/opt v1.0.0
 )
