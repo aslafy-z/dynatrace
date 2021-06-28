37f214413ffa5b4b1748101090a1284921e3f2af
come.to.the.dark.side.cookie@gmail.com <reinhard.pilz@dynatrace.com>
2021-06-28 12:39:06 +0200

    changed tag_filter to required

--- api/config/alerting/profile_severity_rule.go
+++ api/config/alerting/profile_severity_rule.go
@@ -30,7 +30,7 @@ func (me *ProfileSeverityRule) Schema() map[string]*hcl.Schema {
 		"tag_filter": {
 			Type:        hcl.TypeList,
 			Description: "Configuration of the tag filtering of the alerting profile",
-			Optional:    true,
+			Optional:    false,
 			MinItems:    1,
 			Elem: &hcl.Resource{
 				Schema: new(ProfileTagFilter).Schema(),
